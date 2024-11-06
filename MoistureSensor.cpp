9e7f006e936acc7b18a7ae8120660fdba3e7792f
Mityo Draganov <mityodraganow@gmail.com>
2024-11-07 00:59:34 +0200

    driver fixed for modular design

--- MoistureSensor.cpp
+++ MoistureSensor.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <Arduino.h>
 
-// Static instance of the MoistureSensor
+
 static MoistureSensor moistureSensor;
 
 MoistureSensor::MoistureSensor() : readIndex(0), total(0), average(0) {
@@ -11,18 +11,15 @@ MoistureSensor::MoistureSensor() : readIndex(0), total(0), average(0) {
     std::fill(std::begin(readings), std::end(readings), 0);
 }
 
-void MoistureSensor::init() {
-    Serial.println("Initializing Moisture Sensor...");
-    // ADC Configuration: Set width and attenuation
-    adc1_config_width(ADC_WIDTH_BIT_12);  // Set to 12-bit resolution (0-4095)
-    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_11);  // Set attenuation for full-scale voltage (0-3.3V)
+void MoistureSensor::init(int gpioPin) {
+    gpio = gpioPin;  
 }
 
 int MoistureSensor::readValue() {
     // Average multiple readings to smooth the value
     total = 0;
     for (int i = 0; i < NUM_READINGS; i++) {
-        readings[i] = readADC();
+        readings[i] = analogRead(gpio);
         total += readings[i];
     }
     average = total / NUM_READINGS;
@@ -33,13 +30,10 @@ int MoistureSensor::readValue() {
     // Clamp the percentage to be within 0 - 100 range
     percentage = std::max(0, std::min(100, percentage));
 
-    return percentage;  // Return the moisture level as a percentage
+    return percentage; 
 }
 
-int MoistureSensor::readADC() {
-    // Read the ADC value from the moisture sensor (VP pin, GPIO36)
-    return adc1_get_raw(ADC_CHANNEL);  // Read the raw ADC value from channel 0 (GPIO36)
-}
+
 
 const char* MoistureSensor::getType() {
     return "MoistureSensor";  // Return the sensor type as a string
--- MoistureSensor.h
+++ MoistureSensor.h
@@ -3,35 +3,29 @@
 
 #include "../Sensor/Sensor.h"
 #include "../SensorManager/SensorManager.h"
-#include <algorithm>
-#include <iterator>
 #include "driver/adc.h"
 
-class MoistureSensor : public Sensor {
+class MoistureSensor : public Sensor
+{
 public:
-    MoistureSensor();  // Constructor declaration
+    MoistureSensor(); // Constructor declaration
 
-    void init() override;  // Initialize the moisture sensor (ADC setup)
-    int readValue() override;  // Read and return the averaged moisture sensor value
-    const char* getType() override;  // Return the type of the sensor
+    void init(int gpioPin) override; // Initialize the moisture sensor (ADC setup)
+    int readValue() override;        // Read and return the moisture level as a percentage
+    const char *getType() override;  // Return the type of the sensor
 
 private:
-    // GPIO36 (VP) corresponds to ADC1_CHANNEL_0
-    static const adc1_channel_t ADC_CHANNEL = ADC1_CHANNEL_0;
 
     // Calibration values (optional, based on your sensor)
     static const int DRY_SOIL_VALUE = 4095;
     static const int WET_SOIL_VALUE = 1030;
 
-    // Number of readings for smoothing
     static const int NUM_READINGS = 10;
 
-    int readings[NUM_READINGS];  // Array to store multiple readings for averaging
-    int readIndex;  // Current index for reading
-    long total;  // Sum of all readings
-    int average;  // Averaged reading
-
-    int readADC();  // Function to read the raw ADC value
+    int readings[NUM_READINGS]; // Array to store multiple readings for averaging
+    int readIndex;              // Current index for reading
+    long total;                 // Sum of all readings
+    int average;                // Averaged reading
 };
 
 #endif // MOISTURESENSOR_H
